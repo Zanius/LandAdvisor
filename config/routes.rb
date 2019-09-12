7b1c6b1341a22d3411f64cd3dc21def1b3d7ca73
Seth Brown <learc83@gmail.com>
2019-09-12 18:36:22 +0000

    Authenticate non public routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   root 'short_forms#new'
-  
+
   devise_for :users
 
-  resources :short_forms
+  authenticate :user do
+    resources :short_forms, only: [:index, :show, :edit, :update, :destroy]
+  end
+
+  resources :short_forms, only: [:new, :create]
+
+  # resources :short_forms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
