1077cdad1e6a49a169e7f90fa98283bf541df302
Seth Brown <learc83@gmail.com>
2019-09-12 18:28:17 +0000

    Install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'short_forms#new'
+  
+  devise_for :users
+
   resources :short_forms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
